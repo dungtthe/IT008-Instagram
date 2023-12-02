4f0ccddd4e665233686c006704e44369e8e7c17c
Dung Tien The <22520291@gm.uit.edu.vn>
2023-12-02 21:31:40 +0700

    Add project files.

---
+++ IT008-Instagram/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace IT008_Instagram
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
