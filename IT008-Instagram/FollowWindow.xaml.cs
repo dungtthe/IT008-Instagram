1db6e6cd01ea4a328a6434c7a7eaf580ee107913
Vu Binh <22520129@gm.uit.edu.vn>
2023-12-04 23:11:37 +0700

    UI FollowWindown

---
+++ IT008-Instagram/FollowWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace IT008_Instagram
+{
+    /// <summary>
+    /// Interaction logic for FollowWindow.xaml
+    /// </summary>
+    public partial class FollowWindow : Window
+    {
+        public FollowWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAddLink_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
