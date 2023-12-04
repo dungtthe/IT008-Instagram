26267c2f7118837229205c4cf60430dd6fb198c0
Dung Tien The <22520291@gm.uit.edu.vn>
2023-12-04 11:24:30 +0700

    -Xử lý backend

---
+++ IT008-Instagram/QLUser.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xaml.Behaviors.Media;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT008_Instagram
+{
+    public class QLUser
+    {
+        public static ObservableCollection<User> getDaTa()
+        {
+            //new User("TAI KHOAN 2","7891010")
+            ObservableCollection < User > listUser= new ObservableCollection<User>();
+
+            using (FileStream fStream = new FileStream("listUser.txt", FileMode.OpenOrCreate, FileAccess.Read))
+            {
+                using(StreamReader sr = new StreamReader(fStream))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        string[] tkmk = line.Split('|');
+                        User user =new User(tkmk[0], tkmk[1]);
+                        listUser.Add(user);
+                    }
+                }
+            }
+            return listUser;
+        }
+    }
+}
---
+++ IT008-Instagram/User.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace IT008_Instagram
+{
+    public class User : INotifyPropertyChanged
+    {
+        private string taiKhoan;
+        private string matKhau;
+        public string TaiKhoan
+        {
+            get => taiKhoan;
+
+            set
+            {
+                if (taiKhoan != value)
+                {
+                    taiKhoan = value;
+                    OnPropertyChanged(nameof(taiKhoan));
+                }
+            }
+        }
+
+        public string MatKhau
+        {
+            get => matKhau;
+            set
+            {
+                if (matKhau != value)
+                {
+                    matKhau = value;
+                    OnPropertyChanged(nameof(matKhau));
+                }
+            }
+        }
+
+        public User(string tk,string mk)
+        {
+            taiKhoan= tk;
+            matKhau= mk;
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
